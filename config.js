4716b384dc4ae58009e89c98edb9bcf79c572a2c
ayushisiyal <74491355+ayushisiyal@users.noreply.github.com>
2024-03-29 15:59:42 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyATl-BU4TbDul9ZIwKjtyATM-2MESnP0Y8",
+  authDomain: "wily-app1-6c162.firebaseapp.com",
+  databaseURL: "https://wily-app1-6c162-default-rtdb.firebaseio.com",
+  projectId: "wily-app1-6c162",
+  storageBucket: "wily-app1-6c162.appspot.com",
+  messagingSenderId: "269821752477",
+  appId: "1:269821752477:web:1bf9d0fb6415b254e25074"
+}
